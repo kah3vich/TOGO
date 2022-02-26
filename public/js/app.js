7c87a8eaf4dcf963045d47670714b40bf6ff244b
kah3vich <nik.b20@mail.ru>
2022-02-26 17:10:57 +0300

    file

--- public/js/app.js
+++ public/js/app.js
@@ -62,25 +62,164 @@ var indexService = new Swiper(".indexService", {
   }
 });
 $(function () {
-  $('body').css('overflow', 'hidden');
-  gsap.to('.preloading__state', 2, {
+  // $("body").css("overflow", "hidden");
+  gsap.to(".preloading__state", 2, {
     delay: 1,
-    top: '-100%',
+    top: "-100%",
     ease: Expo.easeInOut
   });
-  gsap.to('.preloading__start', 2, {
+  gsap.to(".preloading__start", 2, {
     delay: 1.5,
-    top: '-100%',
+    top: "-100%",
     ease: Expo.easeInOut
   });
   setTimeout(function () {
-    $('.preloading').css('display', 'none');
-    $('body').css('overflow', 'visible');
+    $(".preloading").css("display", "none"); // $("body").css("overflow", "visible");
   }, 3500);
 
   if (window.innerWidth <= 800) {
-    $('.animated').removeClass('animated');
+    $(".animated").removeClass("animated");
   }
-}) // overflow: hidden;
+}); // overflow: hidden;
 // 	overflow: visible;
+
+$("body").on("click", ".reg-controls-password", function () {
+  if ($("#reg__password").attr("type") == "password") {
+    $("#reg__password").attr("type", "text");
+  } else {
+    $("#reg__password").attr("type", "password");
+  }
+
+  return false;
+});
+$("body").on("click", ".reg-controls-passwords", function () {
+  if ($("#reg__passwords").attr("type") == "password") {
+    $("#reg__passwords").attr("type", "text");
+  } else {
+    $("#reg__passwords").attr("type", "password");
+  }
+
+  return false;
+});
+$("#reg__name").on("keyup", function () {
+  checkedInputReg();
+});
+$("#reg__email").on("keyup", function () {
+  checkedInputReg();
+});
+$("#reg__phone").on("keyup", function () {
+  checkedInputReg();
+});
+$("#reg__password").on("keyup", function () {
+  checkedInputReg();
+});
+$("#reg__passwords").on("keyup", function () {
+  checkedInputReg();
+});
+$(".reg__form-btn").on("click", function () {
+  checkedInputReg();
+});
+$(".reg__form-checkbox__label").on("click", function () {
+  checkedInputReg();
+});
+
+var checkedInputReg = function checkedInputReg() {
+  if ($("#reg__name").val() == "") {
+    $(".reg__form-name").css("border", "1px solid red");
+    $(".reg__form-btn").attr("disabled", "disabled");
+  } else {
+    $(".reg__form-name").css("border", "1px solid #4F6BFD");
+    $(".reg__form-btn").removeAttr("disabled");
+  }
+
+  if ($("#reg__email").val() == "") {
+    $(".reg__form-email").css("border", "1px solid red");
+    $(".reg__form-btn").attr("disabled", "disabled");
+  } else {
+    $(".reg__form-email").css("border", "1px solid #4F6BFD");
+    $(".reg__form-btn").removeAttr("disabled");
+  }
+
+  if ($("#reg__phone").val() == "") {
+    $(".reg__form-phone").css("border", "1px solid red");
+    $(".reg__form-btn").attr("disabled", "disabled");
+  } else {
+    $(".reg__form-phone").css("border", "1px solid #4F6BFD");
+    $(".reg__form-btn").removeAttr("disabled");
+  }
+
+  if ($("#reg__password").val() == "") {
+    $(".reg__form-password").css("border", "1px solid red");
+    $(".reg__form-btn").attr("disabled", "disabled");
+  } else {
+    $(".reg__form-password").css("border", "1px solid #4F6BFD");
+    $(".reg__form-btn").removeAttr("disabled");
+  }
+
+  if ($("#reg__passwords").val() == "") {
+    $(".reg__form-passwords").css("border", "1px solid red");
+    $(".reg__form-btn").attr("disabled", "disabled");
+  } else {
+    $(".reg__form-passwords").css("border", "1px solid #4F6BFD");
+    $(".reg__form-btn").removeAttr("disabled");
+  }
+};
+
+$("body").on("click", ".signin-controls-password", function () {
+  if ($("#signin__password").attr("type") == "password") {
+    $("#signin__password").attr("type", "text");
+  } else {
+    $("#signin__password").attr("type", "password");
+  }
+
+  return false;
+});
+$("#signin__email").on("keyup", function () {
+  checkedInputSignin();
+});
+$("#signin__phone").on("keyup", function () {
+  checkedInputSignin();
+});
+$("#signin__password").on("keyup", function () {
+  checkedInputSignin();
+});
+$(".signin__form-btn").on("click", function () {
+  checkedInputSignin();
+});
+
+var checkedInputSignin = function checkedInputSignin() {
+  if ($("#signin__email").val() == "") {
+    $(".signin__form-email").css("border", "1px solid red");
+    $(".signin__form-btn").attr("disabled", "disabled");
+  } else {
+    $(".signin__form-email").css("border", "1px solid #4F6BFD");
+    $(".signin__form-btn").removeAttr("disabled");
+  }
+
+  if ($("#signin__phone").val() == "") {
+    $(".signin__form-phone").css("border", "1px solid red");
+    $(".signin__form-btn").attr("disabled", "disabled");
+  } else {
+    $(".signin__form-phone").css("border", "1px solid #4F6BFD");
+    $(".signin__form-btn").removeAttr("disabled");
+  }
+
+  if ($("#signin__password").val() == "") {
+    $(".signin__form-password").css("border", "1px solid red");
+    $(".signin__form-btn").attr("disabled", "disabled");
+  } else {
+    $(".signin__form-password").css("border", "1px solid #4F6BFD");
+    $(".signin__form-btn").removeAttr("disabled");
+  }
+};
+
+$("#reset__email").on("keyup", function () {
+  if ($("#reset__email").val() == "") {
+    $(".reset__form-email").css("border", "1px solid red");
+    $(".reset__form-btn").attr("disabled", "disabled");
+  } else {
+    $(".reset__form-email").css("border", "1px solid #4F6BFD");
+    $(".reset__form-btn").removeAttr("disabled");
+  }
+});
 ;
--- src/js/index/index.js
+++ src/js/index/index.js
@@ -1,14 +1,12 @@
 var wow = new WOW({
-	mobile: false, 
+	mobile: false,
 });
 wow.init();
 
 for (let i = 1; i <= $(".ind-question__content-i").length; i++) {
 	$(`.ind-question__content-i:nth-child(${i})`).on("click", () => {
 		if ($(`.ind-question__content-i:nth-child(${i})`).hasClass("ind-question__content-i-a")) {
-			$(`.ind-question__content-i:nth-child(${i})`).removeClass(
-				"ind-question__content-i-a"
-			);
+			$(`.ind-question__content-i:nth-child(${i})`).removeClass("ind-question__content-i-a");
 			$(`.ind-question__content-i:nth-child(${i}) img`).css("transform", "rotate(0deg)");
 			$(`.ind-question__content-i:nth-child(${i}) .ind-question__content-text`).addClass(
 				"question-hide"
@@ -16,9 +14,9 @@ for (let i = 1; i <= $(".ind-question__content-i").length; i++) {
 		} else {
 			$(`.ind-question__content-i:nth-child(${i})`).addClass("ind-question__content-i-a");
 			$(`.ind-question__content-i:nth-child(${i}) img`).css("transform", "rotate(45deg)");
-			$(
-				`.ind-question__content-i:nth-child(${i}) .ind-question__content-text`
-			).removeClass("question-hide");
+			$(`.ind-question__content-i:nth-child(${i}) .ind-question__content-text`).removeClass(
+				"question-hide"
+			);
 		}
 	});
 }
@@ -38,9 +36,6 @@ var reviewsSlider = new Swiper(".reviewsSlider", {
 	},
 });
 
-
-
-
 var indexService = new Swiper(".indexService", {
 	navigation: {
 		nextEl: ".ind-service__content-l  .swiper-controls .swiper-button-next",
@@ -51,47 +46,178 @@ var indexService = new Swiper(".indexService", {
 	},
 	keyboard: true,
 	breakpoints: {
-        1400: {
-            slidesPerView: 4,
-        },
+		1400: {
+			slidesPerView: 4,
+		},
 		1100: {
-            slidesPerView: 3,
-        },
+			slidesPerView: 3,
+		},
 		750: {
-            slidesPerView: 2,
-        },
-        0: {
-            slidesPerView: 1,
-        }
-    }
+			slidesPerView: 2,
+		},
+		0: {
+			slidesPerView: 1,
+		},
+	},
 });
 
 $(() => {
-	$('body').css('overflow', 'hidden')
-	gsap.to('.preloading__state', 2, {
+	// $("body").css("overflow", "hidden");
+	gsap.to(".preloading__state", 2, {
 		delay: 1,
-		top: '-100%',
+		top: "-100%",
 		ease: Expo.easeInOut,
-	})
-	gsap.to('.preloading__start', 2, {
+	});
+	gsap.to(".preloading__start", 2, {
 		delay: 1.5,
-		top: '-100%',
+		top: "-100%",
 		ease: Expo.easeInOut,
-	})
+	});
 	setTimeout(() => {
-		$('.preloading').css('display', 'none')
-		$('body').css('overflow', 'visible')
-	}, 3500)
-
-
+		$(".preloading").css("display", "none");
+		// $("body").css("overflow", "visible");
+	}, 3500);
 
 	if (window.innerWidth <= 800) {
-		$('.animated').removeClass('animated')
+		$(".animated").removeClass("animated");
 	}
-
-})
-
-
+});
 
 // overflow: hidden;
 // 	overflow: visible;
+
+$("body").on("click", ".reg-controls-password", function () {
+	if ($("#reg__password").attr("type") == "password") {
+		$("#reg__password").attr("type", "text");
+	} else {
+		$("#reg__password").attr("type", "password");
+	}
+	return false;
+});
+
+$("body").on("click", ".reg-controls-passwords", function () {
+	if ($("#reg__passwords").attr("type") == "password") {
+		$("#reg__passwords").attr("type", "text");
+	} else {
+		$("#reg__passwords").attr("type", "password");
+	}
+	return false;
+});
+
+$("#reg__name").on("keyup", () => {
+	checkedInputReg();
+});
+$("#reg__email").on("keyup", () => {
+	checkedInputReg();
+});
+$("#reg__phone").on("keyup", () => {
+	checkedInputReg();
+});
+$("#reg__password").on("keyup", () => {
+	checkedInputReg();
+});
+$("#reg__passwords").on("keyup", () => {
+	checkedInputReg();
+});
+
+$(".reg__form-btn").on("click", () => {
+	checkedInputReg();
+});
+
+$(".reg__form-checkbox__label").on("click", () => {
+	checkedInputReg();
+});
+
+const checkedInputReg = () => {
+	if ($("#reg__name").val() == "") {
+		$(".reg__form-name").css("border", "1px solid red");
+		$(".reg__form-btn").attr("disabled", "disabled");
+	} else {
+		$(".reg__form-name").css("border", "1px solid #4F6BFD");
+		$(".reg__form-btn").removeAttr("disabled");
+	}
+	if ($("#reg__email").val() == "") {
+		$(".reg__form-email").css("border", "1px solid red");
+		$(".reg__form-btn").attr("disabled", "disabled");
+	} else {
+		$(".reg__form-email").css("border", "1px solid #4F6BFD");
+		$(".reg__form-btn").removeAttr("disabled");
+	}
+	if ($("#reg__phone").val() == "") {
+		$(".reg__form-phone").css("border", "1px solid red");
+		$(".reg__form-btn").attr("disabled", "disabled");
+	} else {
+		$(".reg__form-phone").css("border", "1px solid #4F6BFD");
+		$(".reg__form-btn").removeAttr("disabled");
+	}
+	if ($("#reg__password").val() == "") {
+		$(".reg__form-password").css("border", "1px solid red");
+		$(".reg__form-btn").attr("disabled", "disabled");
+	} else {
+		$(".reg__form-password").css("border", "1px solid #4F6BFD");
+		$(".reg__form-btn").removeAttr("disabled");
+	}
+	if ($("#reg__passwords").val() == "") {
+		$(".reg__form-passwords").css("border", "1px solid red");
+		$(".reg__form-btn").attr("disabled", "disabled");
+	} else {
+		$(".reg__form-passwords").css("border", "1px solid #4F6BFD");
+		$(".reg__form-btn").removeAttr("disabled");
+	}
+};
+
+$("body").on("click", ".signin-controls-password", function () {
+	if ($("#signin__password").attr("type") == "password") {
+		$("#signin__password").attr("type", "text");
+	} else {
+		$("#signin__password").attr("type", "password");
+	}
+	return false;
+});
+
+$("#signin__email").on("keyup", () => {
+	checkedInputSignin();
+});
+$("#signin__phone").on("keyup", () => {
+	checkedInputSignin();
+});
+$("#signin__password").on("keyup", () => {
+	checkedInputSignin();
+});
+$(".signin__form-btn").on("click", () => {
+	checkedInputSignin();
+});
+
+const checkedInputSignin = () => {
+	if ($("#signin__email").val() == "") {
+		$(".signin__form-email").css("border", "1px solid red");
+		$(".signin__form-btn").attr("disabled", "disabled");
+	} else {
+		$(".signin__form-email").css("border", "1px solid #4F6BFD");
+		$(".signin__form-btn").removeAttr("disabled");
+	}
+	if ($("#signin__phone").val() == "") {
+		$(".signin__form-phone").css("border", "1px solid red");
+		$(".signin__form-btn").attr("disabled", "disabled");
+	} else {
+		$(".signin__form-phone").css("border", "1px solid #4F6BFD");
+		$(".signin__form-btn").removeAttr("disabled");
+	}
+	if ($("#signin__password").val() == "") {
+		$(".signin__form-password").css("border", "1px solid red");
+		$(".signin__form-btn").attr("disabled", "disabled");
+	} else {
+		$(".signin__form-password").css("border", "1px solid #4F6BFD");
+		$(".signin__form-btn").removeAttr("disabled");
+	}
+};
+
+$("#reset__email").on("keyup", () => {
+	if ($("#reset__email").val() == "") {
+		$(".reset__form-email").css("border", "1px solid red");
+		$(".reset__form-btn").attr("disabled", "disabled");
+	} else {
+		$(".reset__form-email").css("border", "1px solid #4F6BFD");
+		$(".reset__form-btn").removeAttr("disabled");
+	}
+});
